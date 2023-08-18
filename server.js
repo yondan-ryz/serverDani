3509a00313baf54d685cde3fdb296e4875092456
yondan-ryz <yonathandani76@gmail.com>
2023-08-18 10:27:03 +0700

    main

--- server.js
+++ server.js
@@ -12,28 +12,41 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const secretKey = 'your_secret_key'; // Ganti dengan kunci rahasia yang kuat
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
 
-    // ... (lakukan validasi pengguna dan password di sini)
-
-    // Misalnya, kita akan mengasumsikan jika username dan password adalah "admin"
-    if (username === "admin" && password === "admin") {
-        const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-
-        res.cookie('token', token, { maxAge: 3600000 }); // Simpan token dalam cookie selama 1 jam
-
-        res.json({ token });
-    } else {
-        res.status(401).json({ message: 'Invalid credentials' });
-    }
-});
 
 const connectionString = "postgres://awmhhxgt:yZ1HVE5U6a6WzGJZP8JbMksTuOSzl2sf@batyr.db.elephantsql.com/awmhhxgt";
 const pool = new Pool({ connectionString });
 
 
 const pastor = [];
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const client = await pool.connect();
+        const query = 'SELECT * FROM users WHERE username = $1';
+        const result = await client.query(query, [username]);
+        const user = result.rows[0];
+        client.release();
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // WARNING: Insecure method, not recommended for production
+        if (password === user.password) {
+            const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
+            res.cookie('token', token, { maxAge: 3600000 });
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Terjadi kesalahan saat proses login');
+    }
+});
 
 app.get('/pastor', async (req, res) => {
     try {
@@ -92,22 +105,6 @@ app.put('/pastor/:id', async (req, res) => {
         res.status(500).send('Terjadi kesalahan saat memperbarui status is_completed');
     }
 });
-
-// app.post('/pastor', async (req, res) => {
-//     const { name, content } = req.body;
-//     try {
-//         const client = await pool.connect();
-//         const query = 'INSERT INTO pastor (name, content, category, is_completed) VALUES ($1, $2, $3, $4) RETURNING *';
-//         const values = [name, content, 'Pendidikan', false]; // Set default category and is_completed
-//         const result = await client.query(query, values);
-//         const insertedPost = result.rows[0];
-//         client.release();
-//         res.json(insertedPost);
-//     } catch (error) {
-//         console.error('Error:', error);
-//         res.status(500).send('Terjadi kesalahan saat menyimpan data');
-//     }
-// });
 
 //kategori pendidikan
 app.post('/pastor', async (req, res) => {
