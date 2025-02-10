a972f8e81ee2dcf940b5de59327e324a7979507d
Yonathan Dani <82562810+yondan-ryz@users.noreply.github.com>
2025-02-10 15:42:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,49 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/blacklist', (req, res) => {
-    const response = {
-        errors: null,
-        data: [
-            {
-                idNumber: "1408045207750001",
-                source: "smmf",
-                contactNumber: "08100000269",
-                custName: "Asep",
-                category: "HR_REASONS",
-                blacklistDatetime: "2024-06-06 17:01:12",
-                blacklistUpdateDate: "2024-06-06 17:01:12",
-                reason: "Melanggar kontrak kerja",
-                whitelist: false
-            },
-            {
-                idNumber: "1408045207750001",
-                source: "bsim",
-                contactNumber: "08100000269",
-                custName: "Asep",
-                category: "FRAUD",
-                blacklistDatetime: "2024-06-06 17:01:12",
-                blacklistUpdateDate: "2024-06-06 17:01:12",
-                reason: "Fraud Issue",
-                whitelist: true
-            },
-            {
-                idNumber: "1408045207750001",
-                source: "bsim",
-                contactNumber: "08100000269",
-                custName: "Asep",
-                category: "DANI",
-                blacklistDatetime: "2024-06-06 17:01:12",
-                blacklistUpdateDate: "2024-06-06 17:01:12",
-                reason: "Fraud Issue",
-                whitelist: true
-            }
-        ],
-        transactionId: "2opYog4YMzbic6JcFNQFJJgkn9l"
-    };
-    res.json(response);
-});
-
 app.get('/blacklist/check', (req, res) => {
     const response = {
         errors: null,
